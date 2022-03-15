770d450cb0096c4c815f5f940be83308c8288209
Pham Phu Dien <phamphudien@gmail.com>
2022-03-15 12:11:54 +0700

    update content treatment

--- script.js
+++ script.js
@@ -57,3 +57,60 @@ function changeLanguage(e) {
     document.title = "Chống dịch Covid-19";
   }
 }
+// particlesJS statistical background animated
+particlesJS.load("particles-js", "./assets/particles.json", function () {
+  // console.log("callback - particles.js config loaded");
+});
+
+// Statistical number animated
+function animateNumber(
+  finalNumber,
+  duration = 5000,
+  startNumber = 0,
+  callback
+) {
+  let currentNumber = startNumber;
+  const interval = window.setInterval(updateNumber, 17);
+  function updateNumber() {
+    if (currentNumber >= finalNumber) {
+      clearInterval(interval);
+    } else {
+      let inc = Math.ceil(finalNumber / (duration / 17));
+      if (currentNumber + inc > finalNumber) {
+        currentNumber = finalNumber;
+        clearInterval(interval);
+      } else {
+        currentNumber += inc;
+      }
+      callback(currentNumber);
+    }
+  }
+}
+function numberAnimate() {
+  animateNumber(1158186, 2000, 0, function (number) {
+    const formattedNumber = number.toLocaleString();
+    document.getElementById("new-case-count").innerText = formattedNumber;
+  });
+
+  animateNumber(457948427, 2000, 0, function (number) {
+    const formattedNumber = number.toLocaleString();
+    document.getElementById("total-case-count").innerText = formattedNumber;
+  });
+
+  animateNumber(392046556, 2000, 0, function (number) {
+    const formattedNumber = number.toLocaleString();
+    document.getElementById("recovered-case-count").innerText = formattedNumber;
+  });
+
+  animateNumber(6044143, 2000, 0, function (number) {
+    const formattedNumber = number.toLocaleString();
+    document.getElementById("death-case-count").innerText = formattedNumber;
+  });
+}
+document.addEventListener("DOMContentLoaded", function () {
+  numberAnimate();
+});
+const statisticalNav = document.getElementById("statistical-nav");
+statisticalNav.addEventListener("click", function () {
+  numberAnimate();
+});
